0ad4298ed6bedeeaf4dd2ead9d86adb81ed1ec61
Shaun Starsprung <s@starsprung.com>
2017-12-25 19:08:16 +0000

    Asyncified

--- set-reminder/webtask.js
+++ set-reminder/webtask.js
@@ -2,39 +2,38 @@ const formBody = require('body/form');
 const { parse, toSeconds } = require('iso8601-duration');
 const { MongoClient } = require('mongodb');
 const twilio = require('twilio');
+const { promisify } = require('util');
+
+const formBodyAsync = promisify(formBody);
 
 module.exports = function (context, req, res) {
   const dbUrl = context.data.DB_URL;
 
-  formBody(req, (err, body) => {
-    const {From: phoneNumber, Body: smsBody} = body;
-    const parsedMessage = parseMessage(smsBody);
+  const body = await(formBodyAsync(req));
+  const {From: phoneNumber, Body: smsBody} = body;
+  const parsedMessage = parseMessage(smsBody);
 
-    if (parsedMessage.durationMillis > 0) {
-      const deliveryDate = new Date(Date.now() + parsedMessage.durationMillis).toISOString();
+  if (parsedMessage.durationMillis > 0) {
+    const deliveryDate = new Date(Date.now() + parsedMessage.durationMillis).toISOString();
+    
+    const client = await MongoClient.connect(dbUrl);
+    const db = client.db('reminders');
+    const result = await db.collection('reminders').insertOne({
+      deliveryDate,
+      message: parsedMessage.messageText,
+      sent: false
+    });
+    client.close();
 
-      MongoClient.connect(dbUrl, (err, client) => {
-        const db = client.db('reminders');
-
-        db.collection('reminders').insertOne({
-          deliveryDate,
-          message: parsedMessage.messageText,
-          sent: false
-        }, function(err, result) {
-          client.close();
-
-          const response = createResponse(`Reminder will be delivered at ${deliveryDate}`);
-          res.writeHead(200, { 'Content-Type': 'text/xml'});
-          res.end(response);
-        });
-      });
-    } else {
-      const response = createResponse('Err: first token must be a non-zero duration in ISO8601 format, e.g. P1M2D or PT1H30M. ' + 
-                                      'See http://bit.ly/2DEJ3UN for details.');
-      res.writeHead(200, { 'Content-Type': 'text/xml'});
-      res.end(response);
-    }
-  });
+    const response = createResponse(`Reminder will be delivered at ${deliveryDate}`);
+    res.writeHead(200, { 'Content-Type': 'text/xml'});
+    res.end(response);
+  } else {
+    const response = createResponse('Err: first token must be a non-zero duration in ISO8601 format, e.g. P1M2D or PT1H30M. ' + 
+                                    'See http://bit.ly/2DEJ3UN for details.');
+    res.writeHead(200, { 'Content-Type': 'text/xml'});
+    res.end(response);
+  }
 };
 
 function parseMessage(message) {
