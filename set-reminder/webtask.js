3e2c78ef6fe6bcdca2f0d9d4037cb82563790fe8
Shaun Starsprung <s@starsprung.com>
2017-12-26 11:13:27 +0000

    Refactoring

--- set-reminder/webtask.js
+++ set-reminder/webtask.js
@@ -6,35 +6,32 @@ const { promisify } = require('util');
 
 const formBodyAsync = promisify(formBody);
 
-module.exports = function (context, req, res) {
-  const dbUrl = context.data.DB_URL;
+module.exports = handleRequest;
+  
+async function handleRequest(context, req, res) {
+  const dbUrl = context.secrets.DB_URL;
 
   const body = await(formBodyAsync(req));
   const {From: phoneNumber, Body: smsBody} = body;
   const parsedMessage = parseMessage(smsBody);
 
   if (parsedMessage.durationMillis > 0) {
-    const deliveryDate = new Date(Date.now() + parsedMessage.durationMillis).toISOString();
+    const deliveryDate = new Date(Date.now() + parsedMessage.durationMillis);
     
-    const client = await MongoClient.connect(dbUrl);
-    const db = client.db('reminders');
-    const result = await db.collection('reminders').insertOne({
-      deliveryDate,
-      message: parsedMessage.messageText,
-      sent: false
-    });
-    client.close();
+    try {
+      await saveReminder(dbUrl, deliveryDate, parsedMessage.messageText);
 
-    const response = createResponse(`Reminder will be delivered at ${deliveryDate}`);
-    res.writeHead(200, { 'Content-Type': 'text/xml'});
-    res.end(response);
+      sendResponse(res, `Reminder will be delivered at ${deliveryDate.toISOString()}.`);
+    } catch (e) {
+      console.log(e);
+
+      sendResponse(res, 'Err: something went wrong, try again later.');
+    }
   } else {
-    const response = createResponse('Err: first token must be a non-zero duration in ISO8601 format, e.g. P1M2D or PT1H30M. ' + 
-                                    'See http://bit.ly/2DEJ3UN for details.');
-    res.writeHead(200, { 'Content-Type': 'text/xml'});
-    res.end(response);
+    sendResponse(res, 'Err: first token must be a non-zero duration in ISO8601 format, e.g. P1M2D or PT1H30M. ' + 
+                 'See http://bit.ly/2DEJ3UN for details.');
   }
-};
+}
 
 function parseMessage(message) {
   const [duration, ...tail] = message.split(' ');
@@ -53,10 +50,31 @@ function parseMessage(message) {
   };
 }
 
-function createResponse(message) {
+async function saveReminder(dbUrl, deliveryDate, message) {
+  const client = await MongoClient.connect(dbUrl);
+  //TODO: extract hardcoded values
+  const db = client.db('reminders');
+  const collection = db.collection('reminders');
+
+  await collection.insertOne({
+    deliveryDate,
+    message,
+    sent: false
+  });
+
+  client.close();
+}
+
+function createTwiml(message) {
   const response = new twilio.twiml.MessagingResponse();
   response.message(message);
   const responseString = response.toString();
 
   return responseString;
+}
+
+function sendResponse(res, message) {
+  const response = createTwiml(message);
+  res.writeHead(200, { 'Content-Type': 'text/xml'});
+  res.end(response);
 }
